608d7ed70473415d93388f59eebf7db65fac83d1
Matheusmp3 <167825465+Matheusmp3@users.noreply.github.com>
2025-02-24 19:39:38 -0300

    finalizado

---
+++ Aula-PAM/Geolocaliza/geolocaliza/App.tsx
@@ -0,0 +1,73 @@
+import { useState,useEffect, useRef} from 'react';
+import { View } from 'react-native';
+import {styles} from './style';
+import MapView, {Marker} from 'react-native-maps';
+import {requestForegroundPermissionsAsync, getCurrentPositionAsync, 
+LocationObject,
+watchPositionAsync, 
+Accuracy, 
+LocationAccuracy
+} from 'expo-location';
+
+export default function App() {
+  const [location,setLocation] = useState<LocationObject | null>(null);
+
+  const mapRef = useRef<MapView>(null)
+
+  async function requestLocationPermissions(){
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if(granted){
+      const CurrentPosition = await getCurrentPositionAsync();
+      setLocation(CurrentPosition);
+
+      console.log("Localização atual: ", CurrentPosition)
+    }
+  }
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1
+    }, (response) => {
+      console.log("Nova localização: ", response);
+      setLocation(response);
+      mapRef.current?.animateCamera({
+        pitch: 70,
+        center: response.coords
+      })
+    });
+  },[]);
+
+  return (
+    <View style={styles.container}>
+      {
+
+      location &&
+
+      <MapView
+
+      ref= {mapRef}
+
+      style={styles.map}
+
+      initialRegion={{
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005, 
+      }}>
+
+      <Marker
+      coordinate={{
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+
+      }}
+      ></Marker>
+      </MapView>
+
+      }
+    </View>
+  );
+}
